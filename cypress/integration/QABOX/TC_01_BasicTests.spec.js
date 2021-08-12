2d0d837dc8fb7996eed6d5162f47117df4959697
Ronald Ugas <ronald.ugas@devsu.com>
2021-08-12 17:48:59 -0500

    First test

---
+++ cypress/integration/QABOX/TC_01_BasicTests.spec.js
@@ -0,0 +1,20 @@
+describe('Login Suite', function () {
+  it('Login Test', function () {
+    cy.log(this.test.parent.title);
+    cy.log(this.test.title);
+    cy.log(this.test);
+    //Arrange
+    cy.visit('https://react-redux.realworld.io/#/login?_k=gnue02');
+    //Act
+    cy.get('input[type="email"]').type('ronald8104@gmail.com');
+    cy.get('input[type="password"]').type('user2010');
+    cy.get('button[type="submit"]').click();
+    //Assert
+    //cy.contains('No articles are here... yet.').should('be.visible');
+    cy.contains('Loading...').should('be.visible');
+  });
+
+  it('Second Test', () => {
+    //cy.log(this.test.parent.title);
+  });
+});
